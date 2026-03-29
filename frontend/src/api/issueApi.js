a41aef050e115504d73fd52182fba4a664bad8e5
gitportable <mariam.cd22@bmsce.ac.in>
2026-03-29 13:02:29 +0530

    Add issue management functions to issueApi

--- frontend/src/api/issueApi.js
+++ frontend/src/api/issueApi.js
@@ -1,5 +1,21 @@
+
 import api from "./axios";
 
 export const getIssues = () => api.get("/issues");
+export const createIssue = (data) => api.post("/issues", data);
 export const updateIssueStatus = (id, status) =>
-  api.put(`/issues/${id}/status`, { status });
+  api.put(`/issues/${id}/status`, { status }); 
+export const deleteIssue = (id) => api.delete(`/issues/${id}`);
+export const getProjectIssues = (projectId) =>
+  api.get(`/issues/project/${projectId}`).then((r) => r.data);
+
+export const getMyTasks = () =>
+  api.get("/issues/my-tasks").then((r) => r.data);
+
+export const getMyIssues = () =>
+  api.get("/issues/my-issues").then((r) => r.data);
+export const getIssueStats = (projectId) =>
+  api
+    .get("/issues/stats", projectId ? { params: { projectId } } : {})
+    .then((r) => r.data);
+ 
